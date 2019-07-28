835780ed62f720de628cfecbf7f0116c9944b99f
Simon Appel <simonappel62@gmail.com>
2019-07-28 16:38:14 +0200

    dont write gradients

--- model.py
+++ model.py
@@ -2062,10 +2062,9 @@ class MaskRCNN():
         # Callbacks
         callbacks = [
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
-                                        histogram_freq=1,
+                                        histogram_freq=0,
                                         write_graph=True,
-                                        batch_size=self.config.BATCH_SIZE,
-                                        write_images=True),
+                                        write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
                                             verbose=0, save_weights_only=True),
         ]
